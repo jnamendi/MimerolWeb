15925e0085bdf8fdf5efbf04cc8e919c55d3852b
Viet Nguyen <qviet92@gmail.com>
2019-07-11 17:16:27 +0700

    Update fb dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,7 +17,7 @@ export const environment: Environment = {
     'en-US',
     'es-ES'
   ],
-  facebookProviderKey: '180053686008137',
+  facebookProviderKey: '464417037685516',
   googleProviderKey: '981996347990-6jeg17tp3qcc5p2on8mhkr0voahiuf3f.apps.googleusercontent.com',
   /** Angular debug tools in the dev console
    * https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
