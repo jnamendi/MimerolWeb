9ad6ac48f5311e10d710439780f71061a6148ea9
Hau Nguyen <hau.nguyen@bmbsoft.com.vn>
2019-08-19 10:59:56 +0700

    check convert select menu item

--- src/main/shared/components/menu/shopping-bags/shopping-bags.component.ts
+++ src/main/shared/components/menu/shopping-bags/shopping-bags.component.ts
@@ -52,24 +52,26 @@ export class ShoppingBagsComponent implements OnInit {
   }
 
   onConvertMenuItem = (selectedMenuItems: OrderItem) => {
-    selectedMenuItems.orderItemsRequest.forEach(smi => {
-      if (typeof smi.menuExraItems !== 'undefined') {
-        smi.menuExraItems.forEach(menuItems => {
-          if (menuItems.extraItemType === 1 && menuItems.selectedExtraItem !== null) {
-            menuItems.extraitems = [];
-            menuItems.extraitems.push(menuItems.selectedExtraItem);
-          } else if (menuItems.extraItemType === 1 && menuItems.selectedExtraItem === null) {
-            menuItems.extraitems = [];
-          }
-          if (menuItems.extraItemType === 2 && menuItems.selectedMultiItem.length > 0) {
-            menuItems.extraitems = [];
-            menuItems.extraitems = menuItems.selectedMultiItem;
-          } else if (menuItems.extraItemType === 2 && menuItems.selectedMultiItem.length === 0) {
-            menuItems.extraitems = [];
-          }
-        });
-      }
-    });
+    if (selectedMenuItems && selectedMenuItems.orderItemsRequest) {
+      selectedMenuItems.orderItemsRequest.forEach(smi => {
+        if (typeof smi.menuExraItems !== 'undefined') {
+          smi.menuExraItems.forEach(menuItems => {
+            if (menuItems.extraItemType === 1 && menuItems.selectedExtraItem !== null) {
+              menuItems.extraitems = [];
+              menuItems.extraitems.push(menuItems.selectedExtraItem);
+            } else if (menuItems.extraItemType === 1 && menuItems.selectedExtraItem === null) {
+              menuItems.extraitems = [];
+            }
+            if (menuItems.extraItemType === 2 && menuItems.selectedMultiItem.length > 0) {
+              menuItems.extraitems = [];
+              menuItems.extraitems = menuItems.selectedMultiItem;
+            } else if (menuItems.extraItemType === 2 && menuItems.selectedMultiItem.length === 0) {
+              menuItems.extraitems = [];
+            }
+          });
+        }
+      });
+    }
     return selectedMenuItems;
   }
 
